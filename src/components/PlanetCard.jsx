4b9bfb6630053a75681c320b1471a115a1f74d6b
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-08-18 15:43:33 -0300

    Testando o requisito 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ planetName } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+};
+
+PlanetCard.defaultProps = {
+  planetName: 'Stranger',
+  planetImage: 'Stranger',
+};
+
+export default PlanetCard;
